27be590b411be2cbdfec037d114b7d6861e62fbf
Anjali Tomar <anjali.tomar.jiwaji@gmail.com>
2019-06-24 21:07:27 -0700

    updating post call & response

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 from flask.views import MethodView
 from werkzeug.exceptions import abort
 
@@ -11,12 +11,10 @@ from weathertracker.utils.conversion import (
 app = Flask(__name__)
 from .measurement import Measurement
 from .measurement_store import add_measurement
-from flask import Response
 
 
 class MeasurementsAPI(MethodView):
     # features/01-measurements/01-add-measurement.feature
-    @app.route('/measurements/', methods=['POST'])
     def post(self):
         if not request.is_json:
             abort(400, "please send request in json format")
@@ -25,7 +23,16 @@ class MeasurementsAPI(MethodView):
         if 'timestamp' not in request_data_dic:
             abort(400, "timestamp field can be empty")
 
-        timestamp = request_data_dic.pop('timestamp')
+        try:
+            timestamp_str = request_data_dic.pop('timestamp')
+            timestamp = convert_to_datetime(timestamp_str)
+        except DatetimeConversionException as dce:
+            print("Error: {}".format(dce))
+            abort(400, "improper timestamp format")
+        except Exception as e:
+            print("improper timestamp {}".format(e))
+            abort(400, "invalid timestamp")
+
         metrics = {}
         for metric, value in request_data_dic.items():
             try:
@@ -33,13 +40,17 @@ class MeasurementsAPI(MethodView):
                 metrics[metric] = float(value)
             except ValueError as e:
                 print("value error parsing metric value {}".format(e))
+                abort(400, "illegal metric value")
             except Exception as e:
                 print("error parsing metric value {}".format(e))
+                abort(400, "illegal request")
         add_measurement(Measurement(timestamp, metrics))
 
-        return Response(status=201)
-        # return Response("{'a':'b'}", status=201, mimetype='application/json')
-        # abort(501)
+        response = jsonify()
+        response.status_code = 201
+        response.headers['location'] = '/measurements/{}'.format(timestamp_str)
+        response.autocorrect_location_header = False
+        return response
 
     # features/01-measurements/02-get-measurement.feature
     def get(self, timestamp):
